9a2e3296e96f0168c0bfa7c6db306be8ebc249dc
Marcus Chiu <marcuschiu9@gmail.com>
2017-09-03 18:46:40 -0500

    updated startup.sh again

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-mongod --dbpath /Users/marcus.chiu/ComputerScience/MyProjects/LogTwo/data &
+rm /Users/marcus.chiu/ComputerScience/MyProjects/LogTwo/mongod.log
+
+mongod --dbpath /Users/marcus.chiu/ComputerScience/MyProjects/LogTwo/data --fork --logpath /Users/marcus.chiu/ComputerScience/MyProjects/LogTwo/mongod.log
 npm start --prefix /Users/marcus.chiu/ComputerScience/MyProjects/LogTwo
+
+# kill mongod server after control+c
+kill $(ps aux | grep 'mongod --dbpath /Users/marcus.chiu/ComputerScience/MyProjects/LogTwo/data --fork --logpath /Users/marcus.chiu/ComputerScience/MyProjects/LogTwo/mongod.lo' | grep -v grep | awk '{print $2}')
