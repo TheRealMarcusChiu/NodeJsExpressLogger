f500b04b1e29683b86764f1023cbed6fc5d732e7
Marcus Chiu <marcuschiu9@gmail.com>
2017-09-03 18:29:16 -0500

    are you sure you want to delete log?

--- public/javascripts/log/list.js
+++ public/javascripts/log/list.js
@@ -1,24 +1,29 @@
 function removeLog(logID) {
-    var method = "POST";
-    var url = "/log/delete";
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-    xhr.onreadystatechange = function() { //Call a function when the state changes.
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            if (xhr.responseText === "1") {
-                window.location.href = '/log/list';
+    var result = confirm("Are you sure you want to delete?");
+
+    if (result) {
+        var method = "POST";
+        var url = "/log/delete";
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+        xhr.onreadystatechange = function() { //Call a function when the state changes.
+            if(xhr.readyState === 4 && xhr.status === 200) {
+                if (xhr.responseText === "1") {
+                    window.location.href = '/log/list';
+                }
+                else {
+                    alert("error");
+                }
             }
-            else {
-                alert("error");
-            }
-        }
-    };
+        };
 
-    var params = {
-        "id":logID
-    };
+        var params = {
+            "id":logID
+        };
 
-    xhr.send(JSON.stringify(params));
+        xhr.send(JSON.stringify(params));
+    }
 }
