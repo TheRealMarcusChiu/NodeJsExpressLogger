277187489e67326cf66df6399c738a44f63beec7
Marcus Chiu <marcuschiu9@gmail.com>
2017-09-05 22:42:29 -0500

    shutdown.sh

---
+++ shutdown.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# kill mongod server after control+c
+kill $(ps aux | grep 'mongod' | grep -v grep | awk '{print $2}')
+
+#kill node server
+kill $(lsof -i :3000 | awk '{print $2}')
