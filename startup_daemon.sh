67fb608e83d5f619f9ec7ecd46a036ae0b3f2964
marcus chiu <marcus@marcus.com>
2017-11-02 22:56:48 -0500

    updated startup and shutdown scripts to be run in background

--- startup_daemon.sh
+++ startup_daemon.sh
@@ -5,7 +5,7 @@ DIR_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rm ${DIR_PATH}/log/mongod.log
 
 mongod --dbpath ${DIR_PATH}/data --storageEngine=wiredTiger --fork --logpath ${DIR_PATH}/log/mongod.log
-npm start --prefix ${DIR_PATH}
 
-# kill mongod server after control+c
-kill $(ps aux | grep 'mongod' | grep -v grep | awk '{print $2}')
+cd ${DIR_PATH}
+
+nohup node bin/www > ${DIR_PATH}/log/stdout.txt 2> ${DIR_PATH}/log/stderr.txt &
